33689c8e14acdd30d64cb25bf041f8b43f677004
SunthornK <sunthorn.ko@ku.th>
2024-09-09 14:39:59 +0700

    Fix flake8 docstring

--- statistics.py
+++ statistics.py
@@ -1,15 +1,16 @@
+"""Provide arithmetic statistics functions."""
 from cmath import sqrt
 
 
 def average(data):
     """Return the average of a list of numeric values in data."""
-    if len(data)==0:
+    if len(data) == 0:
         raise ValueError("List must contain at least one value")
     return sum(data)/len(data)
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """Return the population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -18,7 +19,7 @@ def variance(data):
 
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
 
-    :param data: list of numbers for which variance will be computed. 
+    :param data: list of numbers for which variance will be computed.
            Must contain at least one element.
     :returns: population variance of values in data list.
     :raises ValueError: if the data parameter is empty.
@@ -35,10 +36,9 @@ def variance(data):
     if not data:
         raise ValueError("List must contain at least one value")
     avg = average(data)
-    return round(sum([(x-avg)**2 for x in data])/len(data),1)
+    return round(sum([(x-avg)**2 for x in data])/len(data), 1)
 
 
 def stdev(data):
-    """the standard deviation of a list of values"""
+    """Return the standard deviation of a list of values."""
     return sqrt(variance(data))
-
